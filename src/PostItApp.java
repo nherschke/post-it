11b46b99ea10d17fa9642323300ea130182bbc7d
nherschke <nicolas.herschke@gmx.de>
2019-08-08 18:50:54 +0200

    Created basic flow of the program

--- src/PostItApp.java
+++ src/PostItApp.java
@@ -6,12 +6,49 @@ import java.util.Scanner;
 
 public class PostItApp {
     public static void main(String[] args) {
+        boolean quit = false;
         Wall wall = new Wall();
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
 
-        if (scanner.hasNextLine()) {
-            String postNote = scanner.nextLine();
-            wall.add(new PostIt(postNote));
+        System.out.format("Welcome to your post it wall.%n");
+
+        while (!quit) {
+            System.out.format("[p]rint | [n]ew | [r]emove | [s]ave | [l]oad | [q]uit%n");
+            String choice;
+
+            choice = scanner.next();
+
+            switch (choice) {
+                case "p":
+                    for (PostIt p : wall.getPostIts())
+                        p.post(wall);
+                    break;
+
+                case "n":
+                    System.out.print("Note: ");
+                    String text = scanner.next();
+                    wall.add(new PostIt(text));
+                    break;
+
+                case "r":
+
+                case "s":
+
+                case "l":
+
+                case "q":
+                    quit = true;
+                    break;
+
+                default:
+                    System.out.format("Option is not recognized.%n");
+                    break;
+            }
+        }
+
+        if (quit) {
+            scanner.close();
+            System.exit(0);
         }
     }
 }
--- src/Wall.java
+++ src/Wall.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
+
 public class Wall {
+    private ArrayList<PostIt> postIts;
+
+    public Wall() {
+        postIts = new ArrayList<>();
+    }
+
     /**
      * Adds a new post it to the wall for displaying
      * @param postIt The instance of PostIt to add to the wall
      */
     public void add(PostIt postIt) {
-        postIt.post(this);
+        postIts.add(postIt);
+    }
+
+    public ArrayList<PostIt> getPostIts() {
+        return postIts;
     }
 }
